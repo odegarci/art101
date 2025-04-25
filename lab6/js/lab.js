a0a704e3f4571485ef5ffc4bdb159bd194f2eaf4
odette <odegarci@ucsc.edu>
2025-04-25 16:05:18 -0700

    did lab 6 . I made some arrays aand objects as needed on the js file. THen imade sure to ha e the script output be present in the html and modified the image in CSS.

--- lab6/js/lab.js
+++ lab6/js/lab.js
@@ -1,10 +1,43 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js -Lab 6: Arrays and Objects
+// Author: Odette Garcia
+// Date:April 25th, 2025
 
-// Constants
 
-// Functions
+// Lab 6: Arrays and Objects
+
+let myTransport = ["Mercedes-Benz E-Class", "bus", "legs"];
+
+var myMainRide = {
+  make: "Mercedes-Benz",
+  model: "E-CLass",
+  color: "Pewter",
+  year: 2009,
+  age: function() {
+      return 2025 - this.year
+  }
+}
+
+myTransport[1];
+
+myMainRide.year;
+
+
+// Outputs
+
+document.writeln("Riding in my: " + myTransport + "<br>");
+
+document.writeln("My Main Ride: <pre>", 
+  JSON.stringify(myMainRide, null, '\t'), "</pre>");
+
+
+
+
+
+
+
+
+
+
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
 function myFunction(param1, param2) {
