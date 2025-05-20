3d40416c29a6b4b1b321cdce048e42fcb1558571
odette <odegarci@ucsc.edu>
2025-05-19 19:17:59 -0700

    Update lab.js

--- lab11/js/lab.js
+++ lab11/js/lab.js
@@ -1,10 +1,11 @@
 // lab.js - Lab 11: JavaScript Events and Forms
-//
+// jquery to create events, in this came an anagram 
 // Author: Odette Garcia
 // Date: May 15th, 2025
 
 // Script
 
+//characters sorted into alphabetical order
 function sortString(inputString) {
     return inputString.split('').sort().join("");
 }
@@ -12,5 +13,6 @@ function sortString(inputString) {
 $("#submit").click(function(){
     const userName = $("#user-name").val();
   userNameSorted = sortString(userName);
+  //appended new div to output div
     $("#output").html('<div class = "text"><p>' + userNameSorted + '</p></div>');
 })
