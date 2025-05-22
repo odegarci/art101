687dc30bd716fa1d4a8c58401f7552a217e7025f
odette <odegarci@ucsc.edu>
2025-05-21 17:36:56 -0700

    initial upload of lab 12

---
+++ lab12/js/lab.js
@@ -0,0 +1,23 @@
+// lab.js - Lab 12: Conditionals
+// using conditionals to create a Sorting Hat function
+// Author: Odette Garcia
+// Date: May 20th, 2025
+
+// Script
+
+function sortingHat(str) {
+  let len = str.length;
+  let mod = len % 4;
+  if (mod == 0) {
+    return ""
+  }
+  else if (mod == 1) {
+    return ""
+  }
+  else if (mod == 2) {
+    return ""
+  }
+  else if (mod == 3) {
+    return ""
+  }
+}
