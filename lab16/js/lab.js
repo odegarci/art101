1eab0067bb58f194ef2b19f21dc7b2bd083892a5
odette <odegarci@ucsc.edu>
2025-06-05 21:21:37 -0700

    adding lab 16 and testing the xkcd api

---
+++ lab16/js/lab.js
@@ -0,0 +1,31 @@
+// lab.js - Lab 16: JSON and APIs
+// accessing XKCD API
+// Author: Odette Garcia
+// Date: June 5, 2025
+
+const ajaxObj = {
+  url: "https://xkcd.com/info.0.json",
+  data:{},
+  type: "GET",
+  dataType: "json"
+};
+
+$("button").click(function () {
+  $.ajax(ajaxObj)
+    .done(function (data) {
+      console.log(data);
+
+      let title = data.title;
+      let num = data.num; 
+      let desc = data.transcript;
+      let imgUrl = data.img; 
+
+      $("output").html(`<h2>${title}</h2>`);
+      $("output").append(`<img src="${imgUrl}" alt="${data.alt}" title="${data.alt}">`);
+      $("output").append(`<p class='date'>Comic: ${num}</p>`);
+      $("output").append(`<p class='desc'>${desc}</p>`);
+    })
+    .fail(function (xhr, status, error) {
+      console.log("Error:", error);
+    });
+});
